b22c2dcb194bab94c475366e0dc52015f8fb82bc
Johan C. Santana <johancarlosst@gmail.com>
2022-08-11 18:43:42 -0400

    added gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-advice-generator/',
   plugins: [vue()],
   resolve: {
     alias: {
